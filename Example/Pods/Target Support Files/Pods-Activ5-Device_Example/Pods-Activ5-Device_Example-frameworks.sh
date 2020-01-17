abafe8b1a885383299e7ed81bc94ab45a5644ed2
Martin Kuvandzhiev <martinkuvandzhiev@gmail.com>
2020-01-17 13:37:24 -0800

    Build with Swift 5.1.3

--- Example/Pods/Target Support Files/Pods-Activ5-Device_Example/Pods-Activ5-Device_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-Activ5-Device_Example/Pods-Activ5-Device_Example-frameworks.sh
@@ -161,10 +161,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${PODS_ROOT}/../../Activ5Device.framework"
+  install_framework "${PODS_ROOT}/Activ5Device/Activ5Device.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${PODS_ROOT}/../../Activ5Device.framework"
+  install_framework "${PODS_ROOT}/Activ5Device/Activ5Device.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
